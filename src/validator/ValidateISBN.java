075a2d66e3aeffa5cfce78de711cd1e5d67b7c74
Fasust <sebastianf333@googlemail.com>
2019-05-20 16:11:46 +0200

    Refactoring

--- src/validator/ValidateISBN.java
+++ src/validator/ValidateISBN.java
@@ -3,9 +3,8 @@ package validator;
 public class ValidateISBN {
 
     public boolean checkISBN(String isbn) throws NumberFormatException{
-        if(isbn.length() != 10) {
-            throw new NumberFormatException();
-        }
+        if(isbn.length() != 10) throw new NumberFormatException("ISBN must be 10 Digits Long");
+
         int total = 0;
 
         for(int i = 0; i< 10; i++){
