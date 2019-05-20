b3f7661845bf86fb350611c9b510ecff09f2a6ff
Fasust <sebastianf333@googlemail.com>
2019-05-20 16:52:12 +0200

    X in ISBN Refactor

--- src/validator/ValidateISBN.java
+++ src/validator/ValidateISBN.java
@@ -3,13 +3,19 @@ package validator;
 public class ValidateISBN {
 
     public boolean checkISBN(String isbn) throws NumberFormatException{
+        isbn = isbn.toUpperCase();
+
         if(isbn.length() != 10) throw new NumberFormatException("ISBN must be 10 Digits Long");
-        if(!isbn.matches("^[0-9]*$"))throw new NumberFormatException("ISBN must only be Digits");
+        if(!isbn.matches("^[0-9]*[0-9X]$"))throw new NumberFormatException("ISBN must only be Digits");
 
         int total = 0;
 
         for(int i = 0; i< 10; i++){
-            total += (int) isbn.charAt(i) * (10 - i);
+            if(isbn.charAt(i) == 'X'){
+                total += 10;
+                continue;
+            }
+            total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
         }
 
         return total % 11 == 0;
--- test/validator/ValidateISBNTest.java
+++ test/validator/ValidateISBNTest.java
@@ -18,6 +18,18 @@ public class ValidateISBNTest {
     }
 
     @Test
+    public void ISBNEndingInAnXAreValid(){
+        ValidateISBN validator = new ValidateISBN();
+
+        boolean res = validator.checkISBN("012000030X");
+        assertTrue(res,"First: Testing \"The Last of the Mohicans\"");
+
+        assertThrows(NumberFormatException.class,() -> {
+            validator.checkISBN("123X456789");
+        });
+    }
+
+    @Test
     public void checkAInvalidISBN(){
         ValidateISBN validator = new ValidateISBN();
         boolean res = validator.checkISBN("0143126562");
