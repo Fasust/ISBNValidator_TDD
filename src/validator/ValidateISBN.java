d91c3ce12d9b9e540610ab222717cf901d6b0a83
Fasust <sebastianf333@googlemail.com>
2019-05-20 16:10:24 +0200

    ISBN Length Test (Green)

--- src/validator/ValidateISBN.java
+++ src/validator/ValidateISBN.java
@@ -2,7 +2,10 @@ package validator;
 
 public class ValidateISBN {
 
-    public boolean checkISBN(String isbn) {
+    public boolean checkISBN(String isbn) throws NumberFormatException{
+        if(isbn.length() != 10) {
+            throw new NumberFormatException();
+        }
         int total = 0;
 
         for(int i = 0; i< 10; i++){
